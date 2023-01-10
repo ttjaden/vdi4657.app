bdc956ecd897d80a05a5d4d7b165d0bca2a5b154
Tjarko Tjaden <t.tjaden@gmail.com>
2023-01-10 10:37:30 +0100

    set python 3.9

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,10 +1,8 @@
 """
 run these tests with `pytest tests/test_something.py` or `pytest tests` or simply `pytest`
 pytest will look for all files starting with "test_" and run all functions
-within this file. For basic example of tests you can look at our workshop
-https://github.com/rl-institut/workshop/tree/master/test-driven-development.
-Otherwise https://docs.pytest.org/en/latest/ and https://docs.python.org/3/library/unittest.html
-are also good support.
+within this file. For basic example of tests you can look https://docs.pytest.org/en/latest/ 
+and https://docs.python.org/3/library/unittest.html .
 """
 import pytest
 
