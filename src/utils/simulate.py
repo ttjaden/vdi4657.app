b75292bb13e39e2eae035327ce0a665fb154b68d
Tjarko Tjaden <t.tjaden@gmail.com>
2023-01-18 14:53:06 +0100

    added load peak shaving

--- src/utils/simulate.py
+++ src/utils/simulate.py
@@ -21,6 +21,133 @@ class HeatStorage():
         T_sp=T_sp + (1/(self.V_sp*self.c_w))*(P_hp - P_ld - P_loss*(T_sp-self.T_amb))*dt
         return T_sp
 
+# Calculation of electrical grid supply KPIs
+def calc_gs_kpi(P_gs):
+    # to DataFrame
+    profile = pd.DataFrame()
+    profile['P_gs'] = P_gs
+    # grid supply in kWh
+    E_gs = round(profile['P_gs'].mean() * 8760,0)    # kWh
+    # peak load
+    P_gs_max = round(profile['P_gs'].max(),1)        # kW
+    # annual utilization time
+    t_util_a = round(E_gs / P_gs_max,0)              # h
+    return E_gs, P_gs_max, t_util_a
+
+# Calculation of rated power and capacity for peak shaving
+def calc_bs_peakshaving(P_gs0):
+    # Start KPIs
+    E_gs0, P_gs_max0, t_util0 = calc_gs_kpi(P_gs0)
+    # General Parameters
+    dt = 900
+    eta_bat=0.95
+    eta_bat2ac = 0.95
+    eta_ac2bat = 0.95
+    # DataFrames
+    df = pd.DataFrame({'P_bs_discharge_max': [],
+                        'P_bs_charge_max': [],
+                        'C_bs': [],
+                        'E_gs': [],
+                        'P_gs_max': [],
+                        'delta_P_gs': [],
+                        't_util': [],
+                        'E_rate': [],
+                        'E_bs_charge_ac': [],
+                        'E_bs_discharge_ac': [],
+                        'N_full_cylces': []})
+    df.loc[0] = [0, 0, 0, E_gs0, P_gs_max0, 0, t_util0, 0, 0, 0, 0]
+    # Start values
+    delta_P_gs_rel = 0
+    E_rate = 5
+    j = 0
+    # Iteration
+    while E_rate >= 0.20:
+        j = j + 1
+        # add 1% of peak shaving 
+        delta_P_gs_rel = delta_P_gs_rel + 0.01     # %
+        delta_P_gs = P_gs_max0 * delta_P_gs_rel     # kW
+        P_gs_max = P_gs_max0 - delta_P_gs           # kW
+        # calculate battery discharge and grid supply
+        P_bs_discharge = P_gs0 - P_gs_max           # kW
+        P_bs_discharge[P_bs_discharge < 0] = 0      # kW
+        P_bs_charge = P_gs_max - P_gs0              # kW
+        P_bs_charge[P_bs_charge < 0] = 0            # kW
+        P_bs_set = P_bs_discharge - P_bs_charge
+        # Energies
+        E_bs_discharge = []                         # kWh
+        E_bs_discharge.append(P_bs_discharge.iloc[0])
+        for i in range(1,len(P_bs_discharge)):
+            if P_bs_discharge[i] == 0:
+                E_bs_discharge.append(0)
+            else:
+                E_bs_discharge.append(E_bs_discharge[i-1] + P_bs_discharge[i] * 0.25)
+        # Find maximum discharge period in year
+        E_bs_discharge_max = max(E_bs_discharge)
+        # Calculate battery capacity and KPIs
+        C_bs = E_bs_discharge_max / ((eta_bat+((1-eta_bat)/2))*eta_bat2ac*0.8)
+        E_rate = (delta_P_gs / eta_bat2ac) / (C_bs)
+        # Calculate battery charge and soc
+        P_bs = [0]
+        P_bat = [0]
+        SOC = [1]
+        for i in range(1,len(P_bs_set)):
+            if P_bs_set[i] >= 0 and SOC[i-1] > 0.2: # Entladen
+                P_bs.append(P_bs_set[i])
+                P_bat.append(P_bs[i] / ((eta_bat+((1-eta_bat)/2))*eta_bat2ac))
+                SOC.append(SOC[i-1] - (P_bat[i] * 0.25 / C_bs))
+            elif P_bs_set[i] < 0 and SOC[i-1] < 1:  # Laden
+                P_bs.append((max(P_bs_set[i], delta_P_gs*-1)))
+                P_bat.append(P_bs[i] * ((eta_bat+((1-eta_bat)/2))*eta_ac2bat))
+                SOC.append(SOC[i-1] - (P_bat[i] * 0.25 / C_bs))
+            else:
+                P_bs.append(0)
+                P_bat.append(0)
+                SOC.append(SOC[i-1])
+            if SOC[i] > 1:                          # soc correction overload
+                delta_SOC = SOC[i] - 1
+                delta_P = delta_SOC * C_bs / 0.25
+                P_bs[i] = P_bs[i] + delta_P
+                P_bat[i] = P_bs[i] * ((eta_bat+((1-eta_bat)/2))*eta_ac2bat)
+                SOC[i] = 1
+            if SOC[i] < 0.2:                        # soc correction deep discharge
+                delta_SOC = 0.2 - SOC[i]
+                delta_P = delta_SOC * C_bs / 0.25
+                P_bs[i] = P_bs[i] - delta_P
+                P_bat[i] = P_bs[i] / ((eta_bat+((1-eta_bat)/2))*eta_bat2ac)
+                SOC[i] = 0.2
+        # Grid supply
+        P_gs = P_gs0 - P_bs
+        E_gs, P_gs_max, t_util = calc_gs_kpi(P_gs)
+        # Battery
+        data = pd.DataFrame()
+        data['P_bs_charge'] = P_bs
+        data['P_bs_charge'][data['P_bs_charge']>0] = 0
+        E_bs_charge = data['P_bs_charge'].abs().mean()*8760
+        data['P_bs_discharge'] = P_bs
+        data['P_bs_discharge'][data['P_bs_discharge']<0] = 0
+        E_bs_discharge = data['P_bs_discharge'].mean()*8760
+        data['P_bs_charge_dc'] = P_bat
+        data['P_bs_charge_dc'][data['P_bs_charge_dc']>0] = 0
+        E_bs_charge_dc = data['P_bs_charge_dc'].abs().mean()*8760
+        data['P_bs_discharge_dc'] = P_bat
+        data['P_bs_discharge_dc'][data['P_bs_discharge_dc']<0] = 0
+        E_bs_discharge_dc = data['P_bs_discharge_dc'].mean()*8760
+        N_full_cylces = (E_bs_charge_dc + E_bs_discharge_dc)/(2*C_bs) 
+
+        df.loc[j] = [round(max(P_bs),1), 
+                    round(-1*min(P_bs),1),
+                    round(C_bs,1), 
+                    round(E_gs,0), 
+                    round(P_gs_max,1), 
+                    round(P_gs_max0-P_gs_max,1), 
+                    round(t_util,0), 
+                    round(E_rate,2),
+                    round(E_bs_charge,1),
+                    round(E_bs_discharge,1),
+                    round(N_full_cylces,1)
+                    ]
+    return df, P_bs, P_gs, P_gs0
+
 # Calculation of photovoltaic ac power time series
 # normalized to 1 kWp with 1 kW inverter
 def calc_pv(trj, year, type, tilt, orientation):
